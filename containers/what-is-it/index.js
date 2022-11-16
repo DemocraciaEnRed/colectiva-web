b1489986f9522aa320a08a45a9c275e86a094275
Mariano Valdez <marianovaldez92@protonmail.com>
2022-11-16 10:50:19 -0300

    video

--- containers/what-is-it/index.js
+++ containers/what-is-it/index.js
@@ -17,14 +17,41 @@ const StyledWhatIsItSection = styled.section`
   box-sizing:border-box;
 `
 
+const StyledResponsiveVideo = styled.div`
+  position: relative;
+  margin-top: 30px;
+  width: 100%;
+  margin: 0 auto;
+  max-width: 560px;
+  height: auto;
+  display:block;
+  padding-bottom: 56.25%;
+  margin-bottom: 40px;
+  margin-top: 10px;
+  @media (min-width: 1180px) {
+  padding-bottom: 0;
+  height: 315px;
+  width: 560px;
+  }
+`
+
+const StyledIframe = styled.iframe`
+    left:0;
+    top:0;
+    height:100%;
+    width:100%;
+    position:absolute;
+`
+
+
 const WhatIsIt = (props) => {
   if (!props.authContext) return null
   return (
     <StyledWhatIsItSection id='WhatIsIt'>
       <WhatIsItTitle>¿Qué es colectiva?</WhatIsItTitle>
-        <video controls="true">
-          <source src="https://www.youtube.com/watch?v=25wiPu9_V3o" type="video/mp4" />
-        </video>
+        <StyledResponsiveVideo>
+          <StyledIframe width="560" height="315" src="https://www.youtube.com/embed/7gChyZFBqes" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        </StyledResponsiveVideo>      
     </StyledWhatIsItSection>
   )
 }
