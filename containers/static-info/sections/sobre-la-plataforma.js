f64d64e80b2aaa6ff1d8cc9d6652336c06af253c
Mariano Valdez <marianovaldez92@protonmail.com>
2022-11-16 10:02:36 -0300

    cambio sobre la plataforma

--- containers/static-info/sections/sobre-la-plataforma.js
+++ containers/static-info/sections/sobre-la-plataforma.js
@@ -18,18 +18,8 @@ export default () => (
         ¿Qué es Colectiva?
       </StaticInfoBold>
       <StaticInfoP>
-        El país pide un cambio político que nos permita hacerle frente a la crisis económica, de desigualdad, climática y de futuro.
+        Colectiva es la primera plataforma feminista en Latinoamérica para discutir, deliberar, construir y movilizarnos alrededor de las iniciativas que afectarán la vida de las niñas, jóvenes, mujeres y personas #LGBTIQ+ de nuestro país.
       </StaticInfoP>
-      <StaticInfoP>
-        Hoy tenemos frente a nosotras la oportunidad de entrar al Congreso y que nunca sea un espacio cerrado, alejado, inalcanzable e impenetrable que tome en nuestro nombre las decisiones que afectan nuestra vida cotidiana.
-      </StaticInfoP>
-      <StaticInfoP>
-        Desde Artemisas creemos que no hay otra forma posible de construir el país que queremos sin que sea de manera COLECTIVA.
-      </StaticInfoP>
-      <StaticInfoP>
-        Colectiva es la primera plataforma feminista en Latinoamérica para discutir, construir y movilizarnos alrededor de las iniciativas que afectarán la vida de las  niñas, jóvenes, mujeres y personas #LGBTIQ+ de nuestro país.
-      </StaticInfoP>
-
 
     </StyledDiv>
     <StyledDiv>
@@ -37,7 +27,13 @@ export default () => (
         ¿Por qué participar?
       </StaticInfoBold>
       <StaticInfoP>
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+        El país pide un cambio político que nos permita hacerle frente a la crisis económica, de desigualdad, climática y de futuro.
+      </StaticInfoP>
+      <StaticInfoP>
+        Hoy tenemos frente a nosotras la oportunidad de entrar al Congreso y que nunca sea un espacio cerrado, alejado, inalcanzable e impenetrable que tome en nuestro nombre las decisiones que afectan nuestra vida cotidiana.
+      </StaticInfoP>      
+      <StaticInfoP>
+        Desde Artemisas creemos que no hay otra forma posible de construir el país que queremos sin que sea de manera COLECTIVA.
       </StaticInfoP>
     </StyledDiv>
     <StyledDiv>
