163a647ae920501a14e8314d92639f51d2561da6
Guillermo Croppi <guillermocroppi@gmail.com>
2022-11-22 19:00:51 -0300

    New Footer

--- containers/footer/index.js
+++ containers/footer/index.js
@@ -10,16 +10,36 @@ import SocialIcon from '../../elements/social-icon/component'
 
 const FooterWrapper = styled.footer`
   width: 100%;
-  height: 14rem;
+  padding: 4rem 5%;
   background-color: var(--gray);
   display:flex;
-  flex-direction:column;
-  justify-content: flex-end;
+  flex-direction:row;
+  justify-content: space-between;
+  align-items: center;
+  @media (max-width: 760px) {
+    flex-direction: column;
+    padding: 4rem 0;
+  }
   margin-top:10rem;
+`
+
+const FooterImage = styled.img`
+  width: 100%;
+  max-width: 400px;
 `
 
 const Footer = () => (
   <FooterWrapper>
+    <FooterImage src="/static/assets/images/logotipo.png" alt="Colectiva" />
+    <SocialBar>
+      <SocialIcon
+        img={'/static/assets/facebook-icon.svg'}
+        link={'https://www.facebook.com/OrganizacionArtemisas/'} />
+      <SocialIcon img={'/static/assets/twitter-icon.svg'}
+        link={'https://twitter.com/Artemisas_org'} /> 
+      <SocialIcon img={'/static/assets/instagram-icon.svg'}
+        link={'https://www.instagram.com/organizacionartemisas'} /> 
+    </SocialBar>
     <FooterBar>
       <Link href='/info?section=acerca-de'><a>Acerca de</a></Link>
       <Link href='/info?section=contacto'><a>Contacto</a></Link>
@@ -27,13 +47,6 @@ const Footer = () => (
       <Link href='/terminos-y-condiciones'><a>Términos y condiciones</a></Link>
       {/* <Link href='/politicas-de-privacidad'><a>Políticas de privacidad</a></Link> */}
     </FooterBar>
-    <SocialBar>
-      <SocialIcon
-        img={'/static/assets/facebook-icon.svg'}
-        link={'https://www.facebook.com/OrganizacionArtemisas/'} />
-      <SocialIcon img={'/static/assets/twitter-icon.svg'}
-        link={'https://twitter.com/Artemisas_org'} /> 
-    </SocialBar>
   </FooterWrapper>
 )
 
